bf35939ed6488741d86fb386ca07ba160c692fd1
Roaa Alhakimi <58307502+ro5a@users.noreply.github.com>
2022-02-16 15:15:01 +0300

    create page with node js pug method

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Homepage'
+      });
+  });
+  app.set('view engine', 'pug');
+const server = app.listen(7000, () => {
+    console.log(`Express running → PORT ${server.address().port}`);
+  });
